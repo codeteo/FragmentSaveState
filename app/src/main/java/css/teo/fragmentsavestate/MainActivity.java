2c3cfe1e7366b6cca3a906e8f8c9bd7d32f7eb9f
teo <ramones.teo@gmail.com>
2017-01-03 20:39:57 +0200

    Initial commit

---
+++ app/src/main/java/css/teo/fragmentsavestate/MainActivity.java
@@ -0,0 +1,21 @@
+package css.teo.fragmentsavestate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.fl_container) FrameLayout flContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+    }
+}
